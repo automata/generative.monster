75f947131b7239cbf7546c2c2bbd248d0cfaeec7
Vilson Vieira <vilson@void.cc>
2023-06-16 14:17:22 -0300

    Add prompts

--- generative_monster/prompts.py
+++ generative_monster/prompts.py
@@ -5,7 +5,7 @@ PROMPT_SUFFIXES = {
     },
     "acrylic": {
         "source": "https://prompthero.com/prompt/e771e31e3d4",
-        "suffix": " Acrylic art by Anna dittmann, John William Waterhouse, Charlie bowater, Karol bak, Jacek yerka, victo ngai. trending on Artstation, intricate, highly detailed, crispy quality, dynamic lighting, hyperdetailed and realistic. (DreamlikeArt:1. 1), 8k, UHD, HDR, (Masterpiece:1. 5), (best quality:1. 5), Model: Dark Sushi Mix -Mix"
+        "suffix": ", Acrylic art by Anna dittmann, John William Waterhouse, Charlie bowater, Karol bak, Jacek yerka, victo ngai. trending on Artstation, intricate, highly detailed, crispy quality, dynamic lighting, hyperdetailed and realistic. (DreamlikeArt:1. 1), 8k, UHD, HDR, (Masterpiece:1. 5), (best quality:1. 5), Model: Dark Sushi Mix -Mix"
     },
     "dark": {
         "source": "https://prompthero.com/prompt/2e47d07d283",
@@ -17,14 +17,26 @@ PROMPT_SUFFIXES = {
     },
     "max_rive": {
         "source": "https://www.midjourney.com/app/jobs/cf579c17-3918-4287-9aac-d6e81771a151/",
-        "suffix": "Detailed epic shot, by Max Rive, by Dan Mumford, incredible crimson color palette, dramatic cinematic atmosphere, epic composition, ultra contrast, hyper detailed, volumetric, vibrant colored stars, astrology, amazing liquid space matter, anti matter, 16k, 32k"
+        "suffix": ", Detailed epic shot, by Max Rive, by Dan Mumford, incredible crimson color palette, dramatic cinematic atmosphere, epic composition, ultra contrast, hyper detailed, volumetric, vibrant colored stars, astrology, amazing liquid space matter, anti matter, 16k, 32k"
 	},
 	"android": {
 		"source": "https://www.midjourney.com/app/jobs/6fe2bae2-7d60-4b38-ac65-89e99866649b/",
-		"suffix": "Hyper realistic, latex and leather, action pose, cybernetics, robotic character intricately sculpted, gun belt and holster, straps and buckles, military gear, beautiful dynamic composition, photo - realistic techniques, scoutcore, dramatic spotlight horror cinematic lighting, symmetry"
+		"suffix": ", Hyper realistic, action pose, cybernetics, robotic character intricately sculpted, beautiful dynamic composition, photo - realistic techniques, scoutcore, dramatic spotlight horror cinematic lighting, symmetry"
 	},
     "wes_anderson": {
         "source": "",
         "suffix": ", in the style of Wes Anderson, hyper realistic, symmetry, 16k"
+    },
+    "black_and_white": {
+        "source": "",
+        "suffix": ", archillect style, black and white, noir, architect, brutalist"
+    },
+    "graphic_novel": {
+        "source": "",
+        "suffix": ", graphic novel, high detailed"
+    },
+    "gopro_selfie": {
+        "source": "",
+        "suffix": ", gopro, selfie, high definition, hyper realistic"
     }
 }
